3e0d73ede02ff9dab9b7a55d15f561ce5ff3c9d4
Olivia Moldovan <oliviamoldovan56@gmail.com>
2026-05-07 06:34:50 +0000

    Am facut cerinta 2 de la Phase 2

--- monitor_reports.c
+++ monitor_reports.c
@@ -8,12 +8,49 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-void create_monitor.pid()
+void createmonitorpid(const char*district)
 {
-    
+   char fp[256];
+   snprintf(fp,sizeof(fp),"%s/.monitor_pid",district);
+   int fd=open(fp, O_WRONLY| O_CREAT | O_APPEND,0644);
+   if(fd==-1)
+   return ;
+printf("Fisierul monitorpid a fost creat cu succes");
+char buffer[256];
+pid_t p=getpid();
+int lung=snprintf(buffer,sizeof(buffer),"%d",p);
+write(fd,buffer,lung);
+close(fd);
 }
+
+
+ void signals(int sig)
+ {
+
+
+    if(sig==SIGUSR1)
+    {
+        const char msg[]="A fost detectat un raport nou adaugat!";
+        write(STDOUT_FILENO,msg,strlen(msg));
+    }
+    if(sig=SIGINT)
+    {
+       const char msg[]="A fost detectat un semnal";
+       write(STDOUT_FILENO,msg,strlen(msg));
+       unlink(".monitor_pid");
+
+
+    }
+
+
+ }
+
+
+
+
 
 int main()
 {
+  createmonitorpid(".");
     return 0;
 }
